72eb8b21eb89d8884c54f52a4386734114849283
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2025-03-14 10:41:40 +0100

    Integrated i18n boilerplate

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import Alpine from 'alpinejs'
+import AlpineI18n from 'alpinejs-i18n';
+import Alpine from 'alpinejs';
 
-window.Alpine = Alpine
+let locale = 'ua';
 
-Alpine.start()
+let messages = {
+    ua: { cardSection: { head: "привіт", header: "Перепрошуємо", par_one: "ця сторінка ще в розробці", par_two: "приходьте потім" } },
+    en: { cardSection: { head: "hello", header: "Sorry", par_one: "this page is under construction yet", par_two: "please visit us later" } },
+};
+
+document.addEventListener('alpine-i18n:ready', function () {
+    window.AlpineI18n.create(locale, messages);
+});
+
+Alpine.plugin(AlpineI18n);
+
+Alpine.store('locale', {
+    toggle(value) {
+        // AlpineI18n.locale = "en";
+        console.log(value)
+    }
+});
+
+Alpine.start();
